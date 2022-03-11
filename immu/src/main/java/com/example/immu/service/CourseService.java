07a106dcc72afe8747989388e20288a97dc2a357
Immu Saarijärvi <you@example.com>
2022-03-11 18:34:02 +0200

    kurssin txt-tiedostot toimii

--- immu/src/main/java/com/example/immu/service/CourseService.java
+++ immu/src/main/java/com/example/immu/service/CourseService.java
@@ -3,16 +3,41 @@ package com.example.immu.service;
 import com.example.immu.data.Courses;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CourseService {
+
+    StudentFileService myStudentFileService;
+
     private List<Courses> courses = new ArrayList<>();
+
+
+
+    //Kun CourseService olio luodaan springiin, niin haetaan kurssi tiedot filestä
+    public CourseService(){
+        myStudentFileService= new StudentFileService();
+        try {
+            courses = myStudentFileService.readCoursesFromFile();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (NullPointerException f){
+            System.out.println(f.getMessage());
+            f.printStackTrace();
+        }
+    }
 
     //Kurssin lisäys listaan
     public void addCourse(Courses course){
         courses.add(course);
+        try {
+            myStudentFileService.writeCoursesToFile(courses,true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     //Palauttaa kurssilistan
--- immu/src/main/java/com/example/immu/service/StudentFileService.java
+++ immu/src/main/java/com/example/immu/service/StudentFileService.java
@@ -1,5 +1,6 @@
 package com.example.immu.service;
 
+import com.example.immu.data.Courses;
 import com.example.immu.data.Students;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +12,7 @@ import java.util.Scanner;
 @Service
 public class StudentFileService {
 
-
+    //Kirjoitetaan luodut studentit tiedostoon
     public void writeStudentsToFile(List<Students> students, boolean b) throws IOException {
         FileWriter fw = new FileWriter( "students.txt");
 
@@ -28,6 +29,7 @@ public class StudentFileService {
 
     }
 
+    //Luetaan studentit tiedostosta ja lisätään students listaan
     public List<Students> readStudentsFromFile() throws FileNotFoundException {
         Scanner sc = new Scanner(new File("students.txt"));
         List<Students> students = new ArrayList<>();
@@ -42,5 +44,39 @@ public class StudentFileService {
         }
         sc.close();
         return students;
+    }
+
+    //Kirjoitetaan luodut kurssit tiedostoon
+    //Jos koitti intin (c.getCredits) tallettaa toisena, ei tallettanu tietoja oikein. En tierä miksi
+    public void writeCoursesToFile(List<Courses> courses, boolean b) throws IOException {
+        FileWriter fw1 = new FileWriter( "courses.txt");
+
+        for (Courses c: courses) {
+            fw1.write(c.getcName());
+            fw1.write("---");
+            fw1.write(c.getTeacher());
+            System.out.println(c.getCredits());
+            fw1.write("---");
+            fw1.write(c.getCredits()+ System.lineSeparator());
+        }
+        fw1.close();
+
+    }
+
+    //Luetaan kurssi tiedostosta ja lisätään kurssi listaan
+    public List<Courses> readCoursesFromFile() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("courses.txt"));
+        List<Courses> courses = new ArrayList<>();
+
+
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] info = line.split("---");
+            System.out.println(info[0]);
+            Courses c = new Courses(info[0], info[1], Integer.parseInt(info[2]));
+            courses.add(c);
+        }
+        sc.close();
+        return courses;
     }
 }
